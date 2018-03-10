0d129491ac47a05928487646adea2babf93e9592
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2018-03-10 13:43:07 -0800

    Implemented mine function.

--- app.py
+++ app.py
@@ -16,10 +16,35 @@ blockchain = BlockChain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return 'mining'
+    # run proof of work algo to get next proof
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # give a reward for finding the proof
+    # send is 0 to signify that the node is a new coin mined
+    blockchain.new_transaction(
+        sender='0',
+        recipient=node_identifier,
+        amount=1
+    )
+
+    # add new block to chain
+    previous_hash = blockchain.hash(last_block)
+    new_block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': 'New Block Forged',
+        'index': new_block['index'],
+        'transactions': new_block['transactions'],
+        'proof': new_block['proof'],
+        'previous_hash': new_block['previous_hash'],
+    }
+    return jsonify(response), 200
+
 
 @app.route('/transactions/new', methods=['POST'])
-def new_transation():
+def new_transaction():
     """
     """
     params = request.get_json()
@@ -32,6 +57,7 @@ def new_transation():
     index = blockchain.new_transaction(params['sender'], params['recipient'], params['amount'])
     response = {'message': 'Transaction will be added to the block {index}'}
     return jsonify(response), 201
+
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
